b50122fc136c936fed2f411db05e848614a570d5
younggeeks <younggeeks101@gmail.com>
2018-03-21 17:40:02 +0300

    [Fix] fix fetch events action creator

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,5 +1,5 @@
 import {
-    FETCH_EVENTS_FAILED, FETCH_EVENTS_REQUEST, FETCH_EVENTS_SUCCESS,SHOW_SINGLE_EVENT
+    FETCH_EVENTS_FAILED, FETCH_EVENTS_REQUEST, FETCH_EVENTS_SUCCESS, REQUEST_FAILED, SHOW_SINGLE_EVENT
 } from "../constants/action_types";
 import fetch from "cross-fetch"
 export const requestEvents = ()=>({
@@ -23,7 +23,7 @@ const receiveSingleEvent = event=>
     event
 });
 export const eventFetchFailed = error =>({
-    type:FETCH_EVENTS_FAILED,
+    type:REQUEST_FAILED,
     error
 });
 
