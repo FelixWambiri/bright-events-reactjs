215b1b84c509c89a364b41a61d73f05531084db6
younggeeks <younggeeks101@gmail.com>
2018-03-21 17:40:52 +0300

    [Feature] add Bar component to Dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import AuthService from "../../helpers/AuthService";
-import {Redirect} from "react-router-dom";
+import {Bar} from "react-chartjs-2"
+
+const options ={}
+const data = {
+    labels: ["Weddings", "Graduation", "SendOff"],
+    datasets: [{
+        label: 'Number of Attendees',
+        data: [12, 19, 3],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)'
+        ],
+        borderWidth: 1
+    }]
+}
 
 class Dashboard extends Component {
-
     constructor(props) {
         super(props);
         this.Auth = new AuthService()
@@ -20,10 +39,7 @@ class Dashboard extends Component {
     render() {
         return (
             <div>
-                <div className="container ">
-                    <h5 className="centered">My Events Report</h5>
-                    <canvas id="eventsChart"/>
-                </div>
+               <Bar data={data} options={options}/>
 
             </div>
         );
