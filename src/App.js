0cb8d7a0e8f5831f37c24b408c56cb32a08fb1f4
younggeeks <younggeeks101@gmail.com>
2018-04-01 18:25:07 +0300

    [Fix] separate generic api actions for reusability

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ class App extends Component {
                 </div>
             </Router>
         </div>
-
-
     );
   }
 }
--- src/reducers/signup.reducer.js
+++
@@ -1,7 +0,0 @@
-// import {REQUEST_SIGNUP, SIGNUP_FAILED, SIGNUP_SUCCESS} from "../constants/action_types";
-//
-// export default (state={}, action)=>{
-//     switch (action.type){
-//             return state;
-//     }
-// }
