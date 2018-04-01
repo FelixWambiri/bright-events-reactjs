617cb52085e64cc1f9f2bd7a9ae03a64e94aaa8f
younggeeks <younggeeks101@gmail.com>
2018-04-01 18:37:28 +0300

    [Feature #156422596] add map component for showing event map

---
+++ src/components/MapComponent.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import GoogleMapReact from "google-map-react";
+import {MarkerComponent} from "./events/MapMarker";
+
+class MapComponent extends Component {
+    render() {
+        const {coordinates,address,loading} = this.props;
+
+        if(loading){
+            return (
+                <h2>Loading....</h2>
+            )
+        }
+        return (
+            <div style={{height:350,borderRadius:20}} >
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: "AIzaSyD2VmJLaKQpTdow188zsahY20a-bZ0pnAw" }}
+                    center={coordinates}
+                    defaultZoom={15}
+                >
+                    <MarkerComponent
+                        lat={ (coordinates?coordinates.lat:0)}
+                        lng={ coordinates?coordinates.lng:0}
+                        text={address}
+                    />
+                </GoogleMapReact>
+            </div>
+
+        );
+    }
+}
+
+MapComponent.propTypes = {
+    coordinates:PropTypes.object.isRequired,
+    address:PropTypes.string.isRequired,
+    loading:PropTypes.bool
+};
+
+export default MapComponent;
