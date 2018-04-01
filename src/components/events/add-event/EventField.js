94d4558f7d33669e3e03bd6e76e8bf615c2f8f8a
younggeeks <younggeeks101@gmail.com>
2018-04-01 18:44:33 +0300

    [Fix] refactor eventField to include maxlenght prop from description field

--- src/components/events/add-event/EventField.js
+++ src/components/events/add-event/EventField.js
@@ -2,10 +2,10 @@ import React from "react";
 import {withStyles} from "material-ui";
 import {formStyles} from "./styles";
 
-const EventField = ({classes,input,type,placeholder,meta:{touched,dirty,error}})=> {
+const EventField = ({classes,input,type,placeholder,maxlength,meta:{touched,dirty,error}})=> {
     return (
         <div className={`field ${touched?error?'error':'':''}`}>
-            <input {...input} type={type} placeholder={placeholder}/>
+            <input {...input} type={type} placeholder={placeholder} maxlength={maxlength}/>
             {touched &&
             ((error && <span className={classes.errorMessage}>{error}</span>))}
         </div>
