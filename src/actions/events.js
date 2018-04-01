914736bec31a5f8fab4c50800cddd348826fb696
younggeeks <younggeeks101@gmail.com>
2018-04-01 18:28:16 +0300

    [Feature #156422590] add rsvp actions

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,14 +1,23 @@
 import {
-   FETCH_EVENTS_REQUEST, FETCH_EVENTS_SUCCESS, REQUEST_FAILED, SHOW_SINGLE_EVENT
+    EVENT_FORM_ERROR,
+    FETCH_EVENTS_REQUEST, FETCH_EVENTS_SUCCESS, REQUEST_FAILED, RSVP_FAILED, RSVP_LOADING, RSVP_SUCCESS,
+    SHOW_SINGLE_EVENT
 } from "../constants/action_types";
 import fetch from "cross-fetch"
+import {requestFailed, requestStarted} from "./api.actions";
+import MapService from "../helpers/MapService";
+import {fetchedCoordinates} from "./map.actions";
+import history from "../helpers/history";
+import {savedEvent, savingEvent} from "./new_event";
+import ApiService from "../helpers/ApiService";
+import AuthService from "../helpers/AuthService";
 export const requestEvents = ()=>({
     type:FETCH_EVENTS_REQUEST
 });
 
-export const showSingleEvent = event =>({
-    type:SHOW_SINGLE_EVENT,
-    event
+export const addEventFailed = error =>({
+    type:EVENT_FORM_ERROR,
+    error
 });
 
 const receiveEvents = events=>
@@ -22,32 +31,83 @@ const receiveSingleEvent = event=>
     type:SHOW_SINGLE_EVENT,
     event
 });
-export const eventFetchFailed = error =>({
-    type:REQUEST_FAILED,
-    error
+const rsvpSuccess = ()=>
+    ({
+    type:RSVP_SUCCESS
 });
 
+const rsvpLoading = ()=>({
+    type:RSVP_LOADING
+})
+
+const rsvpFailed = (error)=>({
+    type:RSVP_FAILED,
+    error
+})
 
 export const fetchEvents  = ()=>{
     return dispatch =>{
-        dispatch(requestEvents())
+        dispatch(requestStarted())
         return fetch('http://localhost:5000/api/v1/events/')
             .then(response=>response.json())
             .then(json=>dispatch(receiveEvents(json.events)))
             .catch(error=>{
-                dispatch(eventFetchFailed(error.message))
+                dispatch(requestFailed(error.message))
             })
     }
 }
+
+const fetchCoordinates = address =>{
+    return MapService(address)
+        .then(resp=>{
+            const { lat, lng } = resp.results[0].geometry.location;
+           return {lat,lng}
+        })
+        .catch(()=>{
+            return `Could Not Show Map For ${address}`
+
+        })
+};
+
+export const rsvp = (event)=>{
+    const user = new AuthService().getUser()
+    const data = {
+        event,
+        user_id:user.sub
+    }
+    return dispatch =>{
+        dispatch(rsvpLoading());
+        ApiService.events.rsvp(data)
+            .then(()=>{
+                dispatch(rsvpSuccess());
+            })
+            .catch(response=>{
+                try{
+                    response.then(error=>dispatch(rsvpFailed(error.message)));
+                }catch (error){
+                    // response.then(error=>dispatch(rsvpFailed(error.message)));
+                    dispatch(rsvpFailed("Network Request Failed"))
+                }
+
+            })
+    }
+};
 
 export const fetchSingleEvent  = (id)=>{
     return dispatch =>{
         dispatch(requestEvents());
         return fetch(`http://localhost:5000/api/v1/events/${id}`)
             .then(response=>response.json())
-            .then(json=>dispatch(receiveSingleEvent(json.event)))
+            .then(json=>{
+                fetchCoordinates(json.event.address)
+                    .then(data=>{
+                        dispatch(fetchedCoordinates(data))
+                        dispatch(receiveSingleEvent(json.event))
+                    })
+
+            })
             .catch(error=>{
-                dispatch(eventFetchFailed(error.message))
+                dispatch(requestFailed(error.message))
             })
     }
 }
