47882307f5f09ed7732ad4271bc348697067290c
younggeeks <younggeeks101@gmail.com>
2018-04-01 19:02:00 +0300

    [Chore] refactor error reducer to clear error when new request starts

--- src/reducers/error.js
+++ src/reducers/error.js
@@ -1,15 +1,17 @@
 import {
     FETCH_EVENTS_SUCCESS,
-    LOGIN_SUCCESS, REQUEST_FAILED, REQUEST_STARTED
+    LOGIN_SUCCESS, REQUEST_FAILED, REQUEST_STARTED, RSVP_FAILED, RSVP_LOADING
 } from "../constants/action_types";
 
 export default (error='', action)=>{
     switch (action.type){
         case REQUEST_FAILED:
+        case RSVP_FAILED:
             return action.error;
         case LOGIN_SUCCESS:
         case FETCH_EVENTS_SUCCESS:
         case REQUEST_STARTED:
+        case RSVP_LOADING:
             return '';
         default:
             return error;
