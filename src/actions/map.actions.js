21054fe4f339c61a4cd6a05ac51c8d1ab8c7dc68
younggeeks <younggeeks101@gmail.com>
2018-04-01 18:30:33 +0300

    [Feature #156422596] add map actions

---
+++ src/actions/map.actions.js
@@ -0,0 +1,22 @@
+import {FETCH_MAP_SUCCESS} from "../constants/action_types";
+import MapService from "../helpers/MapService";
+import {requestFailed, requestStarted} from "./api.actions";
+
+export  const fetchedCoordinates = (coordinates)=>({
+    type:FETCH_MAP_SUCCESS,
+    coordinates
+});
+
+export const fetchCoordinates = (address)=>{
+    return dispatch=>{
+        dispatch(requestStarted())
+       MapService(address)
+            .then(resp=>{
+                const { lat, lng } = resp.results[0].geometry.location;
+                dispatch(fetchedCoordinates({lat, lng}))
+            })
+           .catch(error=>{
+               dispatch(requestFailed(`Could Not Show Map For <b>${address}</b>`))
+           })
+    }
+}
